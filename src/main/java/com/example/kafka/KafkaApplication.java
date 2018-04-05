3b8d34855aa301f19824bc5f71561f1b26aebe77
Krystian Zybala <kontakt@kzybala.pl>
2018-04-05 21:28:49 +0200

    Add application runner for payload.

--- src/main/java/com/example/kafka/KafkaApplication.java
+++ src/main/java/com/example/kafka/KafkaApplication.java
@@ -1,11 +1,25 @@
 package com.example.kafka;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Component;
 
 @SpringBootApplication
 public class KafkaApplication {
 
+	@Component
+	public static class PageViewEventSource implements ApplicationRunner {
+
+		@Override
+		public void run(final ApplicationArguments args) throws Exception {
+
+		}
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(KafkaApplication.class, args);
 	}
