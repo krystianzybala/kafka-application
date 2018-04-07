54432685095e02b6b4e51e4d247a337ff9060a37
Krystian Zybala <kontakt@kzybala.pl>
2018-04-07 11:40:04 +0200

    Refactoring

--- src/main/java/com/example/kafka/KafkaApplication.java
+++ src/main/java/com/example/kafka/KafkaApplication.java
@@ -1,13 +1,11 @@
 package com.example.kafka;
 
-import lombok.extern.log4j.Log4j2;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.stream.annotation.EnableBinding;
 
 @SpringBootApplication
 @EnableBinding(AnalyticsBinding.class)
-@Log4j2
 public class KafkaApplication {
     public static void main(String[] args) {
         SpringApplication.run(KafkaApplication.class, args);
